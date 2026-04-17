a6ac043b9f6c280d6f28a2a7fb339825c7693107
Benji Franck <benjamin.franck@gmail.com>
2026-04-17 13:08:45 -0600

    add a deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import "@abgov/web-components";
 
